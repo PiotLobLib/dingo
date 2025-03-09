c7178c9dd1f41becfa2f4b227b1431d54c47a25c
PiotLobLib <piotr.loba33@gmail.com>
2025-03-09 15:40:55 +0100

    add templates

--- maths/views.py
+++ maths/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 class Calculator:
@@ -9,21 +11,45 @@ class Calculator:
     @staticmethod
     def add(request, a, b):
         a, b = int(a), int(b)
-        return HttpResponse(a + b)
+        wynik = a + b
+        c = {"a": a, "b": b, "operacja": "+", "wynik": wynik}
+        return render(
+            request=request,
+            template_name="maths/main.html",
+            context=c
+        )
 
     @staticmethod
     def sub(request, a, b):
         a, b = int(a), int(b)
-        return HttpResponse(a - b)
+        wynik = a - b
+        c = {"a": a, "b": b, "operacja": "-", "wynik": wynik}
+        return render(
+            request=request,
+            template_name="maths/main.html",
+            context=c
+        )
 
     @staticmethod
     def mul(request, a, b):
         a, b = int(a), int(b)
-        return HttpResponse(a * b)
+        wynik = a * b
+        c = {"a": a, "b": b, "operacja": "*", "wynik": wynik}
+        return render(
+            request=request,
+            template_name="maths/main.html",
+            context=c
+        )
 
     @staticmethod
     def div(request, a, b):
         a, b = int(a), int(b)
         if b == 0:
             return HttpResponse("Nie dziel przez 0")
-        return HttpResponse(a / b)
+        wynik = a / b
+        c = {"a": a, "b": b, "operacja": "/", "wynik": wynik}
+        return render(
+            request=request,
+            template_name="maths/main.html",
+            context=c
+        )
