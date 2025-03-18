52691e1e75e4c60745e3f6d59bfd0d19138c1316
PiotLobLib <piotr.loba33@gmail.com>
2025-03-18 19:46:05 +0100

    add posts app + model migrations

---
+++ posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Author(models.Model):
+    nick = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nick
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
