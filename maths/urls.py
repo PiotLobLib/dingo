598d93f859b3a22ee02b395e15fe30b2cc01afdd
PiotLobLib <piotr.loba33@gmail.com>
2025-03-18 21:20:36 +0100

    new templates to maths app

--- maths/urls.py
+++ maths/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from maths.views import Calculator
+from maths.views import Calculator, maths_list, math_details
 
+app_name = "maths"
 urlpatterns = [
     path('', Calculator.math),
     path('add/<a>/<b>', Calculator.add),
     path('sub/<a>/<b>', Calculator.sub),
     path('mul/<a>/<b>', Calculator.mul),
     path('div/<a>/<b>', Calculator.div),
+    path('histories/', maths_list, name="list"),
+    path('histories/<int:id>', math_details, name="details"),
 ]
--- maths/views.py
+++ maths/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context
 from django.contrib import messages
+
+from maths.models import Math
 
 
 class Calculator:
@@ -68,3 +69,21 @@ class Calculator:
             request=request,
             template_name="maths/operation.html",
             context=c)
+
+
+def maths_list(request):
+    maths = Math.objects.all()
+    return render(
+        request=request,
+        template_name="maths/list.html",
+        context={"maths": maths}
+    )
+
+
+def math_details(request, id):
+    math = Math.objects.get(id=id)
+    return render(
+        request=request,
+        template_name="maths/details.html",
+        context={"math": math}
+    )
